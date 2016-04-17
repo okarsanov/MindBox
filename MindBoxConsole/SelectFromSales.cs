e9bf27a78e0732fc221dd5f5f395f2249fcf2339
normann <normann8@yandex.ru>
2016-04-18 01:52:58 +0400

    AddQuery

---
+++ MindBoxConsole/SelectFromSales.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MindBoxConsole
+{
+    class SelectFromSales
+    {
+        private String select = "SELECT productid, COUNT (datetime1) AS FirstPurchase FROM Sales WHERE datetime1 IN (" +
+	                                    "SELECT MIN(datetime1) as MinDateTime FROM Sales" + 
+	                                    "GROUP BY customerid" +
+                                ")GROUP BY productid";
+    }
+}
